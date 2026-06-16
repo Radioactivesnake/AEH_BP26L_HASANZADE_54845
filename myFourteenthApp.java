a8db621c3e47c0fada7f06e668e5020040ff71d6
Nigar Hasanzade <119549882+Radioactivesnake@users.noreply.github.com>
2026-06-16 23:08:38 +0200

    Create myFourteenthApp class for vehicle rental demo

---
+++ myFourteenthApp.java
@@ -0,0 +1,6 @@
+public class myFourteenthApp {
+    public static void main(String[] args) {
+        System.out.println("Nigar Hasanzade - Lab 14 - Vehicle Rental System");
+        System.out.println("Run RentalTest to see  demo.");
+    }
+}
